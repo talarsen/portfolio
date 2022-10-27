a95cda9a390ede70c8a3093cf8ad238ee3fd4010
Tara Larsen <talarsen74@gmail.com>
2022-10-27 11:46:37 -0700

    make change on site, add docker to redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,6 +2,5 @@
 cd portfolio
 # make sure git repo in VPS has the latest changes from the main branch on GH
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip3 install -r requirements.txt
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
