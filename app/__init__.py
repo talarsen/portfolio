e605f7ed707b4593e62a2630a748b193a33a618b
Tara Larsen <talarsen74@gmail.com>
2022-11-03 13:33:27 -0700

    fix tests from partner

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from peewee import*
 import datetime
 import json
 from playhouse.shortcuts import model_to_dict
+import re
 
 load_dotenv()
 app = Flask(__name__)
@@ -35,9 +36,35 @@ class TimelinePost(Model):
 mydb.connect()
 mydb.create_tables([TimelinePost])
 
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    try:
+        name = request.form['name']
+    except Exception as e:
+        return "Invalid name", 400
+    else:
+        if name == '':
+            return "Invalid name", 400
+        
+    try:
+        email = request.form['email']
+    except Exception as e:
+        return "Invalid email", 400
+    else:
+        if email == '':
+            return "Invalid email", 400
+        
+    try:
+        content = request.form['content']
+    except Exception as e:
+        return "Invalid content", 400
+    else:
+        if content == '':
+            return "Invalid content", 400
 
-
-
+    if not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email):
+        return "Invalid email", 400
+    
 os.getenv("API_KEY") 
 educationData = [
     {
@@ -61,7 +88,6 @@ educationData = [
         "degree": "Bachelor of Science: Parks and Natural Resources"
     },
 ]
-
 
 @app.route('/')
 def landingPage():
@@ -170,14 +196,7 @@ def timeline():
     created_at.desc())]
     return render_template('timeline.html', title="MLH Fellow - Timeline", url=os.getenv("URL"), timeline_posts=timeline_posts)
 
-# timeline POST route 
-@app.route('/api/timeline_post', methods=['POST'])
-def post_time_line_post():
-    name = request.form['name']
-    email = request.form['email']
-    content = request.form['content']
-    timeline_post = TimelinePost.create(name=name, email=email,content=content)
-    return model_to_dict(timeline_post)
+
 
 #timeline GET route
 @app.route('/api/timeline_post', methods=['GET'])
